68f9173d807d401a94ca1cdff85c33dc079f3d2c
Avinash Bachhav <abachhav@xpanxion.com>
2019-01-09 12:28:58 +0530

    ngv4 added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using client.project.web.host.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace client.project.web.host.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            //return View("~/index.html");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/LoginDTO.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace client.project.web.host.Models
+{
+    public class LoginDTO
+    {
+        public string UserName { get; set; }
+        public string Password {get; set;}
+    }
+}
