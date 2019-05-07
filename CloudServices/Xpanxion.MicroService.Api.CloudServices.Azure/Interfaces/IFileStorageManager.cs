927e5cf6813fbc7d0b84b8c6246cb7533236538c
Mahesh Deore <mdeore@xpanxion.com>
2019-05-07 15:58:57 +0530

    first commit

--- CloudServices/Xpanxion.MicroService.Api.CloudServices.Azure/Interfaces/IFileStorageManager.cs
+++ CloudServices/Xpanxion.MicroService.Api.CloudServices.Azure/Interfaces/IFileStorageManager.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Xpanxion.MicroService.Api.CloudServices.Azure.Interfaces
 {
     public interface IFileStorageManager
     {
+        Task AddFile(string fileName);
     }
 }
