05029dc9e0db06b9d11ec6ad9e22feb9605cfa35
Kishor Jagtap <kjagtap@xpanxion.com>
2019-07-08 10:56:49 +0530

    AzureServiceBusController checkin

---
+++ Service/Xpanxion.MicroService.Api/Controllers/AzureServiceBusController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Xpanxion.MicroService.Api.Integration.Contracts;
+using Xpanxion.MicroService.Api.Integration.RequestHandler.Interfaces;
+
+
+namespace Xpanxion.MicroService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AzureServiceBusController : BaseApiController
+    {
+        private readonly AppSettings _appSettings;
+        public AzureServiceBusController(IRequestHandlerProvider requestHandlerProvider, IOptions<AppSettings> appSettings) : base(requestHandlerProvider)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+    }
+}
